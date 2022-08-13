d8b5edf8c117277bcc97ebdd433abb057bb528e8
Habeeb David <habeebdh1@outlook.com>
2022-08-13 01:42:59 +0100

    Styling Mobile view.
    Add menu image in mobile view.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+if (window.innerWidth < 700) {
+  document.querySelector("#content aside img").src =
+    "./images/image-hero-mobile.png";
+} else {
+  document.querySelector("#content aside img").src =
+    "./images/image-hero-desktop.png";
+}
 const menu = document.getElementsByClassName("menu");
 for (let i = 0; i < menu.length; i++) {
   menu[i].addEventListener("mouseover", function () {
@@ -7,3 +14,12 @@ for (let i = 0; i < menu.length; i++) {
     this.children[0].children[0].src = "./images/icon-arrow-down.svg";
   });
 }
+
+document.getElementById("menu-img").addEventListener("click", function () {
+  let src = this.src;
+  if (src.includes("close")) {
+    this.src = "./images/icon-menu.svg";
+  } else {
+    this.src = "./images/icon-close-menu.svg";
+  }
+});
