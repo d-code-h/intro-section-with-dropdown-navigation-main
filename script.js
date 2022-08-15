e56db244117ffd57a9db7ee396e5ce497bbadfe5
Habeeb David <habeebdh1@outlook.com>
2022-08-15 16:07:25 +0100

    Goal Met.

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ document.getElementById("menu-img").addEventListener("click", function () {
   let src = this.src;
   if (src.includes("close")) {
     this.src = "./images/icon-menu.svg";
-    // document.querySelector("#full__menu.mobile").style.display = "none";
     document.querySelector("header #container").style.display = "none";
     document.getElementById("transparent").style.display = "none";
   } else {
     this.src = "./images/icon-close-menu.svg";
     document.getElementById("transparent").style.display = "block";
     document.querySelector("header #container").style.display = "contents";
-    // document.querySelector("#full__menu.mobile").style.display = "block";
   }
 });
